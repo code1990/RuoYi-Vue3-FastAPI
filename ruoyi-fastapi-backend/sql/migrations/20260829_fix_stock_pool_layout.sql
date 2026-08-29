caace743b5042141000d21c44fb3a403d4178528
s199005107012 <s1332177151@sina.com>
2026-08-29 15:49:33 +0800

    separate stock pool views

--- ruoyi-fastapi-backend/sql/migrations/20260829_fix_stock_pool_layout.sql
+++ ruoyi-fastapi-backend/sql/migrations/20260829_fix_stock_pool_layout.sql
@@ -3,5 +3,5 @@ SET component = 'Layout', menu_type = 'M', update_time = NOW(), update_by = 'adm
 WHERE menu_id = 2053;
 
 UPDATE sys_menu
-SET component = 'stock/pool/stock', update_time = NOW(), update_by = 'admin'
+SET component = 'pool/stock', update_time = NOW(), update_by = 'admin'
 WHERE parent_id = 2053;
